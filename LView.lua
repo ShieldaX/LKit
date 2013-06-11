9f117a8137db462a34399e70e7297ecb650b31cc
ShieldaX <shieldax@gmail.com>
2013-06-11 23:05:19 +0800

    add view class constructor

--- LView.lua
+++ LView.lua
@@ -10,15 +10,24 @@ local LViewMT = {__index = LView}
 local _id = 0  -- ID for vie
 
 --- Class constructor method
-function LView.new()
+-- @frame Init view with frame.
+function LView.new(frame)
+    --assert frame is a display rect
+    local width, height = frame.width or 100, frame.height or 100
+    -- background is used for stretching frame( group object ).
+    local background = display.newRect(0, 0, width, height)
+    background:setFillColor(255, 255, 255, 255)
     local instance = 
         {
             _id = _id + 1,
-            _subViews = {},
+            --_subViews = {},
             --_superView = nil,
-            --_layer = nil,
-            
+            _bg = background,
+            _frame = display.newGroup(),
+            _bounds = display.newGroup(),
         }
+    instance._frame:insert(background) -- puts BG in bottom layer and
+    instance._frame:insert(instance._bounds) -- bounds above BG.
 --
 
     return setmetatable( instance, LViewMT )
@@ -26,16 +35,36 @@ end
 --
 
 -- Manage subViews in self.
+-- Insert view as new subview.
 function LView:addView(view, zIndex)
+    local layer = self._layer
     -- assert.isA(LView)
-    -- zIndex should be a nil or integer value
+    -- zIndex should be a nil or integer value。
+    layer:insert(zIndex, view)
+    self[view.name] = view
 end
 --
 
-function LView:removeView(atIndex)
-    --
+-- Remove subView from self.
+-- @subView Name or zIndex of subView to remove.
+function LView:removeView(subView)
+    local layer= self._layer
+    object = layer[subView] or self[subView]
+    if not object then return false end
+    layer:remove(object)
+    return true
 end
 --
 
+-- Move subView to new Index
+function LView:moveViewToIndex(subView, zIndex)
+    local layer = self._layer
+    if zIndex < 1 then zIndex = 1 elseif zIndex > layer.numChildren then zIndex = layer.numChildren end
+    object = layer[subView] or self[subView]
+    layer:insert(zIndex, object)
+end
+--
+
+--
 
 return LView
