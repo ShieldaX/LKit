0dd4996d3a5ff6b99de1a6f229069766e5f80d1a
ShieldaX <shieldax@gmail.com>
2013-06-16 16:54:27 +0800

    fill app class

---
+++ LApplication.lua
@@ -0,0 +1,45 @@
+-- ========================================
+-- LApplication.lua
+-- Singleton, application loop controller.
+--
+-- ========================================
+
+local application = {}
+--local application_metatable = {__index = application}
+
+-- Instance reference
+local instance = nil
+
+-- Constructor function.
+function application.new()
+    if instance then
+        return false
+    else
+        instance = application
+    end
+end
+--
+
+-- Get singleton instance.
+function application:getInstance()
+    if not instance then
+        return self.new()        
+    else
+        return instance
+    end
+end
+--
+
+function application:start()
+end
+
+function application:stop()
+end
+
+function application:suspend()
+end
+
+function application:resume()
+end
+
+return application
