b5574bbdd9129d8ad41214afb0b1416eefe2623e
ShieldaX <shieldax@gmail.com>
2013-06-16 16:54:52 +0800

    add views

--- LView.lua
+++ LView.lua
@@ -11,25 +11,30 @@ local _id = 0  -- ID for each view.
 
 --- Class constructor method.
 -- @frame Init view with frame.
-function LView.new(frame)
-    --assert frame is a display rect
-    assert(type(frame) == "table", "frame should be a table.")
-    local width, height = frame.width or 100, frame.height or 100
+function LView.new(opt)
+    assert(type(opt) == "table", "option must be a table")
+    _id = _id + 1
+    local width, height = opt.width or 100, opt.height or 100
     -- background is used for stretching frame( group object ).
     local background = display.newRect(0, 0, width, height)
-    background:setFillColor(255, 255, 255, 255) -- set default bg color-- white.
+    local backgroundColor = opt.backgroundColor or {255, 255, 255, 255}
+    background:setFillColor(unpack(backgroundColor))
     if not DEBUG then background.alpha = 0 end
     local instance = 
         {
-            _id = _id + 1,
-            --_subViews = {},
-            --_superView = nil,
+            name = opt.name,
+            _id = _id,
+            subviews = {},
+            --superview = nil,
             _background = background,
-            _frame = display.newGroup(),
-            _bounds = display.newGroup(),
+            _backgroundColor = backgroundColor,
+            frame = display.newGroup(),
+            bounds = display.newGroup(),
+            _gestureRecognizers = {},
         }
-    instance._frame:insert(background) -- make background **rect** as frame skeleton and
-    instance._frame:insert(instance._bounds) -- lay bounds **group** above background.
+    --instance.frame._backgroundColor = {255, 255, 255}
+    instance.frame:insert(background) -- make background **rect** as frame skeleton and
+    instance.frame:insert(instance.bounds) -- lay bounds **group** above background.
 
     return setmetatable( instance, LViewMT )
 end
@@ -39,21 +44,25 @@ end
 -- Manage subViews in self.
 --
 -- Insert view as new subview.
+-- @view Subview to be insert.
+-- *zIndex Display order of new subview.
 function LView:addView(view, zIndex)
-    local bounds = self._bounds
-    -- assert.isA(LView)
-    if not type(self[view.name]) == "nil" then return false end
-    -- zIndex should be a nil or integer value.
-    bounds:insert(zIndex, view)
-    self[view.name] = view
+    --assert(view and isView(view), "Subview must be a LView")
+    --assert(type(view.name) == "string", "")
+    assert(view.superview ~= self, "Subview already exists, use 'moveViewToIndex' to change display order")
+    
+    local bounds = self.bounds
+    bounds:insert(zIndex or bounds.numChildren + 1, view.frame)
+    view.superview = self
+    if type(view.name) == "string" then self.subviews[view.name] = view end
 end
 --
 
 -- Remove subView from self.
--- @subView Name or zIndex of subView to remove.
-function LView:removeView(subView)
+-- @subview Name or zIndex of subView to remove.
+function LView:removeView(subview)
     local bounds= self._bounds
-    object = bounds[subView] or self[subView]
+    object = bounds[subView] or self.subviews[subview]
     if not object then return false end
     bounds:remove(object)
     return true
@@ -61,23 +70,42 @@ end
 --
 
 -- Move subView to new Index.
-function LView:moveViewToIndex(subView, zIndex)
+-- @
+function LView:moveViewToIndex(subview, toIndex)
     local bounds = self._bounds
-    if zIndex < 1 then zIndex = 1 elseif zIndex > bounds.numChildren then zIndex = bounds.numChildren end
-    object = bounds[subView] or self[subView]
-    bounds:insert(zIndex, object)
+    if toIndex < 1 then toIndex = 1 elseif toIndex > bounds.numChildren then toIndex = bounds.numChildren end
+    object = bounds[subview] or self[subview]
+    bounds:insert(toIndex, object)
 end
 --
 -- ---
 
 -- Set background color
--- @colorT Table of color to set to.
-function LView:setBackgroundColor(colorTable, keepAlpha)
+-- @colorTable Table of color to set to.
+-- @transparent:boolean Keep background be transparent or not.
+function LView:setBackgroundColor(colorTable, transparent)
     --local ori_color = self._bgc
     self._background:setFillColor(unpack(colorTable))
-    if not keepAlpha then self._background.alpha = 1 end
-    --self._bgc = colorT
+    if not transparent then self._background.alpha = 1 end
+    self._bounds.backgroundColor = colorTable
 end
+--
+
+-- Get current view's root view -- window.
+function LView:getWindow()
+    local view = self
+    while view._superview do -- if view has super view,
+       view = view._superview -- then try its superview alternatively,
+    end
+    -- until view doesn't have superview.
+    return view
+end
+--
+
+-- Check if the view's descendant.
+-- @view The view to check.
+function LView:isDescendantOfView(view)
+end    
 --
 
 return LView
--- main.lua
+++ main.lua
@@ -5,8 +5,55 @@ print("LKit Lib")
 
 DEBUG = true
 
+-- ---
+-- dump arbitrary variables include table
+--
+function print_r ( t )
+--{{{
+    local print_r_cache={}
+        local function sub_print_r(t,indent)
+            if (print_r_cache[tostring(t)]) then
+                print(indent.."*"..tostring(t))
+            else
+                print_r_cache[tostring(t)]=true
+            if (type(t)=="table") then
+                for pos,val in pairs(t) do
+                    if (type(val)=="table") then
+                        print(indent.."["..pos.."] => "..tostring(t).." {")
+                        sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
+                        print(indent..string.rep(" ",string.len(pos)+6).."}")
+                    elseif (type(val)=="string") then
+                        print(indent.."["..pos..'] => "'..val..'"')
+                    else
+                        print(indent.."["..pos.."] => "..tostring(val))
+                    end
+                end
+            else
+                print(indent..tostring(t))
+            end
+        end
+    end
+
+    if (type(t)=="table") then
+        print(tostring(t).." {")
+        sub_print_r(t,"  ")
+        print("}")
+    else
+        sub_print_r(t,"  ")
+    end
+    print()
+--}}}
+end
+
 local View = require("LView")
 
 local testView = View.new({width = 200, height = 100})
+local toInsert = View.new({name = "foo", width = 50, backgroundColor = {255, 255, 0}})
 
-print("testView internal id: ", testView._id)
+print("internal ids: ", testView._id, toInsert._id)
+
+testView:addView(toInsert)
+
+testView.frame.x = display.contentCenterX
+
+print_r(testView)
