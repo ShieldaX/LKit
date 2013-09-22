ce2449b6d4976b8b994fd66507550738e915719b
ShieldaX <shieldax@gmail.com>
2013-09-22 21:27:29 +0800

    lbutton proto

--- LButton.lua
+++ LButton.lua
@@ -2,13 +2,53 @@
 -- Button maintainer
 --
 
-local Button = {}
-local ButtonMT = {__index = Button}
+-- ------
+-- LIBRARIES
+-- ------
 
--- Constructor method
-function Button.new(opts)
-    local newButton = {}
-    
-    return setmetatable( newButton, ButtonMT )
+local LView = require("LView")
+
+-- ------
+-- CLASS
+-- ------
+
+local LButton = LView:subclass("LButton")
+
+-- ------
+-- CONSTANTS
+-- ------
+
+-- ------
+-- VARIABLES
+-- ------
+
+-- ------
+-- FUNCTIONS
+-- ------
+
+-- ------
+-- ONTOLOGY FUNCTIONS
+-- ------
+
+function LButton:initialize(opt)
+  LView.initialize(self, opt)
+  local roundedRect = display.newRoundedRect(self.bounds, 0, 0, 160, 60, 12)
+  roundedRect.strokeWidth = 2
+  roundedRect:setFillColor(255, 255, 255)
+  roundedRect:setStrokeColor(0, 122, 255)
+  local label = display.newText(self.bounds, "Label", 0, 0, native.systemFont, 30)
+  label:setTextColor(0, 122, 255)
+  label.x = roundedRect.x
+  label.y = roundedRect.y
+  self.bounds:addEventListener("touch", self)
 end
 
+function LButton:touch(event)
+  -- touched
+  if event.phase == "ended" then print("touchUp") end
+end
+
+function LButton:setLabel()
+end
+
+return LButton
