8a273c99f570e5680ec93b35fca7c1fb6cf2d809
ShieldaX <shieldax@gmail.com>
2013-09-22 21:28:42 +0800

    arrange source code

--- LView.lua
+++ LView.lua
@@ -38,18 +38,18 @@ function LView:initialize(opt)
   
   local width, height = opt.width or 100, opt.height or 100
   --frame is, a group object, used for define position and size in superview.
-  self._frame = display.newGroup()
+  self.frame = display.newGroup()
   --bounds is, also a display group, used for define self content position and size in frame.
-  self._bounds = display.newGroup()
+  self.bounds = display.newGroup()
   -- background is, a rect, used for touch handle
-  self.background = display.newRect(self._frame, 0, 0, width, height) -- make background **rect** as frame skeleton and
+  self.background = display.newRect(self.frame, 0, 0, width, height) -- make background **rect** as frame skeleton and
   self.backgroundColor = opt.backgroundColor or {255, 255, 255, 255} -- color table
-  self.background:setFillColor(unpack(backgroundColor))
+  self.background:setFillColor(unpack(self.backgroundColor))
   
   self.subviews = {}
   self.gestureRecognizers = {}
   
-  self._frame:insert(self._bounds) -- lay bounds **group** above background.
+  self.frame:insert(self.bounds) -- lay bounds **group** above background.
 end
 
 --[[
@@ -58,20 +58,20 @@ end
 -- @param *zIndex Display order of new subview.
 ]]
 function LView:addView(view, zIndex)
-    --assert(view and isView(view), "Subview must be a LView")
-    --assert(type(view.name) == "string", "")
-    assert(view.superview ~= self, "subview already exists, use 'moveViewToIndex' to change display order")
-    assert(getmetatable(view._frame) == getmetatable(display.getCurrentStage()), "try to insert an invalid view")
-    assert(string.find(view.name, "_", 1) ~= 1, "can't resolve '_' prefixed subview." )
-    -- ensure view name doesn't contain "-", or it will hurt nested reference.
-    assert(not string.find(view.name, "-", 1), "can't resolve '-' contained subview." )
-    local bounds = self._bounds
-    bounds:insert(zIndex or bounds.numChildren + 1, view._frame)
-    view.superview = self
-    if type(view.name) == "string" then
-      self.subviews[view.name] = view
-      self[view.name] = view
-    end
+  --assert(view and isView(view), "Subview must be a LView")
+  --assert(type(view.name) == "string", "")
+  assert(view.superview ~= self, "subview already exists, use 'moveViewToIndex' to change display order")
+  assert(getmetatable(view.frame) == getmetatable(display.getCurrentStage()), "try to insert an invalid view")
+  assert(string.find(view.name, "_", 1) ~= 1, "can't resolve '_' prefixed subview." )
+  -- ensure view name doesn't contain "-", or it will hurt nested reference.
+  assert(not string.find(view.name, "-", 1), "can't resolve '-' contained subview." )
+  local bounds = self.bounds
+  bounds:insert(zIndex or bounds.numChildren + 1, view.frame)
+  view.superview = self
+  if type(view.name) == "string" then
+    self.subviews[view.name] = view
+    self[view.name] = view
+  end
 end
 --
 
@@ -80,22 +80,22 @@ end
 -- @param subview Name or zIndex of subView to remove.
 ]]
 function LView:removeView(subview)
-    local bounds= self._bounds
-    local object = self.subviews[subview]
-    if not object then
-        object = bounds[subview]
-        subview = self:nameOfView(subview)
-    else
-        object = object._frame
-    end
-    if object then
-        local ref = self.subviews[subview]
-        ref.superview = nil
-        self.subviews[subview] = nil
-        object:removeSelf()
-        object = nil
-        --print_r(ref)
-    end
+  local bounds= self.bounds
+  local object = self.subviews[subview]
+  if not object then
+      object = bounds[subview]
+      subview = self:nameOfView(subview)
+  else
+      object = object.frame
+  end
+  if object then
+      local ref = self.subviews[subview]
+      ref.superview = nil
+      self.subviews[subview] = nil
+      object:removeSelf()
+      object = nil
+      --print_r(ref)
+  end
 end
 --
 
@@ -112,16 +112,16 @@ end
 -- @
 ]]
 function LView:moveViewToIndex(subview, toIndex)
-    local bounds = self._bounds
-    if toIndex < 1 then toIndex = 1 elseif toIndex > bounds.numChildren then toIndex = bounds.numChildren end
-    local object = self.subviews[subview]
-    if not object then
-        object = bounds[subview]
-    else
-        object = object._frame
-    end
-    display.getCurrentStage():insert(object) -- take object out to shared stage
-    bounds:insert(toIndex, object) -- re-insert object at new index
+  local bounds = self.bounds
+  if toIndex < 1 then toIndex = 1 elseif toIndex > bounds.numChildren then toIndex = bounds.numChildren end
+  local object = self.subviews[subview]
+  if not object then
+      object = bounds[subview]
+  else
+      object = object.frame
+  end
+  display.getCurrentStage():insert(object) -- take object out to shared stage
+  bounds:insert(toIndex, object) -- re-insert object at new index
 end
 --
 
@@ -144,13 +144,13 @@ end
 -- @param index Subview's index.
 ]]
 function LView:nameOfView(index)
-    local object = self._bounds[index]
-    if object then    
-        for name, view in pairs(self.subviews) do
-            if object == view._frame then return name end
-        end
+  local object = self.bounds[index]
+  if object then    
+    for name, view in pairs(self.subviews) do
+        if object == view.frame then return name end
     end
-    return false
+  end
+  return false
 end
 --
 
@@ -158,13 +158,13 @@ end
 -- Get current view's root view -- window.
 ]]
 function LView:getWindow()
-    local view = self
-    while view.superview do -- if view has super view,
-       view = view.superview -- then try its superview alternatively,
-    end
-    -- until view doesn't have superview.
-    return view
-    --return self.superview:getWindow()
+  local view = self
+  while view.superview do -- if view has super view,
+     view = view.superview -- then try its superview alternatively,
+  end
+  -- until view doesn't have superview.
+  return view
+  --return self.superview:getWindow()
 end
 --
 
@@ -173,12 +173,12 @@ end
 -- @param targetView The view to check.
 ]]
 function LView:isDescendantOfView(targetView)
-    local view = self
-    while view.superview do -- if view has super view,
-        view = view.superview
-        if view == targetView then return true end
-    end
-    return false
+  local view = self
+  while view.superview do -- if view has super view,
+      view = view.superview
+      if view == targetView then return true end
+  end
+  return false
 end
 --
 
@@ -196,10 +196,10 @@ end
 -- @param transparent:boolean Keep background be transparent or not.
 ]]
 function LView:setBackgroundColor(colorTable, transparent)
-    --local ori_color = self._bgc
-    self._background:setFillColor(unpack(colorTable))
-    if not transparent then self._background.alpha = 1 end
-    self._backgroundColor = colorTable
+  --local ori_color = self._bgc
+  self.background:setFillColor(unpack(colorTable))
+  if not transparent then self.background.alpha = 1 end
+  self.backgroundColor = colorTable
 end
 --
 
--- main.lua
+++ main.lua
@@ -1,7 +1,13 @@
 ---- main.lua ----
 
+-- Hide the status bar
+display.setStatusBar( display.HiddenStatusBar )
+
+-- Set the background to white
+display.setDefault( "background", 255, 255, 255 )
+
 print() 
-print("LKit Lib")
+print("LKit Lib Demo")
 
 -- ---
 -- dump arbitrary variables include table
@@ -9,35 +15,36 @@ print("LKit Lib")
 function print_r ( t )
 --{{{
     local print_r_cache={}
-        local function sub_print_r(t,indent)
-            if (print_r_cache[tostring(t)]) then
-                print(indent.."*"..tostring(t))
+    local function sub_print_r(t,indent)
+      if (print_r_cache[tostring(t)]) then
+        print(indent.."*"..tostring(t))
+      else
+        print_r_cache[tostring(t)]=true
+        if (type(t)=="table") then
+          for pos,val in pairs(t) do
+            if (type(pos)=="table") then pos = tostring(pos) end
+            if (type(val)=="table") then
+              print(indent.."["..pos.."] => "..tostring(t).." {")
+              sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
+              print(indent..string.rep(" ",string.len(pos)+6).."}")
+            elseif (type(val)=="string") then
+              print(indent.."["..pos..'] => "'..val..'"')
             else
-                print_r_cache[tostring(t)]=true
-            if (type(t)=="table") then
-                for pos,val in pairs(t) do
-                    if (type(val)=="table") then
-                        print(indent.."["..pos.."] => "..tostring(t).." {")
-                        sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
-                        print(indent..string.rep(" ",string.len(pos)+6).."}")
-                    elseif (type(val)=="string") then
-                        print(indent.."["..pos..'] => "'..val..'"')
-                    else
-                        print(indent.."["..pos.."] => "..tostring(val))
-                    end
-                end
-            else
-                print(indent..tostring(t))
+              print(indent.."["..pos.."] => "..tostring(val))
             end
+          end
+        else
+          print(indent..tostring(t))
         end
+      end
     end
 
     if (type(t)=="table") then
-        print(tostring(t).." {")
-        sub_print_r(t,"  ")
-        print("}")
+      print(tostring(t).." {")
+      sub_print_r(t,"  ")
+      print("}")
     else
-        sub_print_r(t,"  ")
+      sub_print_r(t,"  ")
     end
     print()
 --}}}
@@ -46,19 +53,28 @@ end
 local View = require("LView")
 local Window = require("LWindow")
 local Linear = require("LLinear")
+local Button = require("LButton")
 
 local win = Window {name = "window"}
 
-local box = Linear {name = "h-box", width = 200, direction = "vertical"}
-box:setBackgroundColor({255, 0, 255})
+local box = Linear {name = "hbox", width = 200, direction = "vertical"}
+box:setBackgroundColor({255, 204, 0})
 
-local v = View {name = "kill", height = 50, backgroundColor = {255, 255, 0}}
+local v = View {name = "gray", height = 50, backgroundColor = {142, 142, 147}}
 
-local vv = View {name = "double_kill", height = 25, backgroundColor = {0, 255, 255}}
+local vv = View {name = "blue", height = 25, backgroundColor = {52, 170, 220}}
+
+local btn = Button {
+    name = "new_button",
+    width = 160,
+    height = 60,
+    backgroundColor = {255, 255, 255, 0}
+  }
 
 win:addView(box)
 box:addView(v)
 box:addView(vv)
+box:addView(btn)
 
 print(box:nameOfView(2))
 print(vv:isDescendantOfView(box))
@@ -68,8 +84,8 @@ print(box.name)
 --print_r(foobar)
 timer.performWithDelay(2600, function()
   --box:removeView(1)
-  print(box.double_kill.name)
-  print(box.kill.name)
+  print(box.blue.name)
+  print(box.gray.name)
     --foobar:removeView(1)
     --print_r(foobar)
     --foobar:moveViewToIndex(1, 2)
@@ -77,6 +93,7 @@ timer.performWithDelay(2600, function()
     --print_r(bar:isDescendantOfView(foobar))
     --print(foobar:nameOfView(5))
     --foobar:removeView('bar')
+  print_r(box.gray)
 end)
 
 --[[
