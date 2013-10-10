cbb5df892660df6fdd506f3b8f957d5f83452814
ShieldaX <shieldax@gmail.com>
2013-10-10 16:24:17 +0800

    desgin gesture recongnizer

--- LGestrueRecognizer.lua
+++ LGestrueRecognizer.lua
@@ -1,7 +1,25 @@
 -- LGestureRecognizer.lua
 
-local LGestureRecognizer = {}
+-- http://jessewarden.com/2012/07/finite-state-machines-in-game-development.html
 
+local LGestureRecognizer = class 'LGestureRecognizer'
+
+LGestureRecognizer.static.State = {
+    Possible = 1,
+    Began = 2,
+    Changed = 3,
+    Ended = 4,
+    Cancelled = 5,
+    Failed = 6,
+    Recognized = 4
+  }
+
+local State = StateMachine:new()
+State:addState("possible", {from="*"})
+State:addState("began", {from="possible"})
+State:addState("changed", {from="began", enter = onEnterChanged})
+State:setInitialState("possible")
+  
 function LGestureRecognizer:touch(event)
   print('try to parse', event.name)
 end
