9354000a921807a1b535b210d8f66a902b5e556a
ShieldaX <shieldax@gmail.com>
2013-11-19 18:59:16 +0800

    exchange file

--- Button/Label.lua
+++ Button/Label.lua
@@ -1,20 +1,21 @@
 -----------------------------------------------
--- @class: Input
--- @file Input.lua - v0.0.1 (2013-09)
--- wrapper of native input field
+-- @class: Label
+-- @file Label.lua - v0.0.1 (2013-09)
+-- Most used control widget class, stateful
 -----------------------------------------------
+-- created at: 2013-11-11 10:50:13
 
 -- ======
 -- LIBRARIES
 -- ======
---local util = require 'util'
---local class = require 'middleclass'
+local util = require 'util'
+local class = require 'middleclass'
 local View = require 'View'
 
 -- ======
 -- CLASS
 -- ======
-local Input = View:subclass('Input')
+local Label = View:subclass('Label')
 
 -- ======
 -- CONSTANTS
@@ -36,54 +37,47 @@ local SOY = display.screenOriginY
 -- ======
 
 -- ------
--- Initializing a Input Control
+-- Initializing a Label Control
 -- ------
 
 --- Instance constructor
--- @param api Intent table for construct new instance.
-function Input:initialize(api)
-  -- class custom api:
-  --api.backgroundColor = {255, 255, 255, 0} -- hide background rect
-  api.backgroundColor = {0, 0, 0, 0} -- hide background rect
+-- @param opt Intent table for construct new instance.
+function Label:initialize(opt)
+  -- class custom opt:
+  --opt.backgroundColor = {255, 255, 255, 0} -- hide background rect
+  opt.backgroundColor = {0, 0, 0, 120} -- hide background rect
+  self.tintColor = opt.tintColor or {0, 0, 0, 255}
+
   -- instantiation
-  View.initialize(self, api)
+  View.initialize(self, opt)
 
-  self.tintColor = api.tintColor or {0, 0, 0, 255}
-  self.size = api.size or 30
-  self.font = api.font or native.systemFontBold
-  self.defaultText = api.defaultText
+  -- rect with border
+  local insetTop, insetRight, insetBottom, insetLeft = opt.insetTop or 0, opt.insetRight or 0, opt.insetBottom or 0, opt.insetLeft or 0
+  local width = self.background.contentWidth - (insetLeft + insetRight)
+  local height = self.background.contentHeight - (insetTop + insetBottom)
 
-  -- layout real text field
-  local frame = self.frame
-  local left = frame.contentBounds.xMin
-  local top = frame.contentBounds.yMin
-  local width = frame.contentWidth
-  local height = frame.contentHeight
-  local field = native.newTextField( left, top, width, height )
-  --field.inputType = api.inputType
-  --field.isSecure = api.isSecure
-
-  self.field = field
-  field:addEeventListener("userInput", self)
+  -- text label
+  local labelText = opt.text or opt.name
+  local label = display.newText {
+    parent = self.bounds,
+    x = width*.5, y = height*.5,
+    width = width, height = height,
+    text = labelText,
+    font = opt.font or native.systemFontBold,
+    fontSize = opt.fontSize or 30,
+    align = "center"
+  }
+  label:setTextColor(unpack(self.tintColor))
+  self.contentView = label
 end
 
-function Input:userInput(event)
-  print("user input just happen")
+function Label:setText(text)
+  local label = self.contentView
+  if label then
+    label.text = text
+  else
+    print(" no label yet ")
+  end
 end
 
-function Input:animation(animation)
-  -- set frame to target position
-  -- then transition native field to act real animation
-end
-
--- Roll input text back to default or none
-function Input:rollBack(clear)
-  self.field.text = clear and  '' or self.defaultText
-end
-
-function Input:setText(text)
-  assert(type(text) == "string")
-  self.field.text = text
-end
-
-return Input
+return Label
