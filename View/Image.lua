13bb990bdad6161fba2d87cae2b7b6b6d854e0be
ShieldaX <shieldax@gmail.com>
2013-12-04 18:20:54 +0800

    init image class

--- View/Image.lua
+++ View/Image.lua
@@ -32,11 +32,13 @@ local SOY = display.screenOriginY
 -- VARIABLES
 -- ======
 
--- Internal identifier
-
 -- ======
 -- FUNCTIONS
 -- ======
+
+local function rectEqualsRect()
+  -- body
+end
 
 -- ------
 -- Initializing a Image Object
@@ -47,10 +49,44 @@ local SOY = display.screenOriginY
 function Image:initialize(api)
   View.initialize(self, api)
   local bounds = self.bounds
-  bounds:insert(api.image)
+  
+  self.image = api.image
+
+  local _img = display.newImageRect(bounds, pathToImage, api.width, api.height)
   --sizeToFit(self.background.contentWidth)
-  ...
+  
 end
 
+function Image:setImage(image)
+  if self.image then
+    image:removeSelf()
+  end
+  local bounds = self.bounds
+  self.image = display.newImageRect( bounds, filename, [baseDirectory], width, height )
+end
+
+-- adjust frame while content updating
+-- update background size
+local function adjustFrame()
+  local background = self.background
+  local frame = self.frame
+  local width, height = frame.width, frame.height
+  print(frame.width, frame.height, frame.contentWidth, frame.contentHeight)
+  -- content size is always lte actual size
+  background.width = width
+  background.height = height
+  background.x = width*.5
+  background.y = height*.5
+end
+
+function Image:sizeThatFits()
+  local bounds = self.bounds
+  return bounds.contentWidth, bounds.contentHeight
+end
+
+function Image:sizeToFit()
+  local width, height = self:sizeThatFits()
+  local background = self.background
+end
 
 return Image
