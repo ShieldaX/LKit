2004e8d338e3bee2eb14efcaf47b38c7a02c3552
ShieldaX <shieldax@gmail.com>
2013-12-18 18:29:55 +0800

    fill gesture class

--- Control/Gesture.lua
+++ Control/Gesture.lua
@@ -2,6 +2,7 @@
 -- @class Gesture
 -- @file Gesture.lua - v0.0.1 (2013-09)
 -- An abstract base class for concrete gesture-recognizer classes, Pan, Flick, Pinch.
+-- Use FSM (inite-state machine)
 -----------------------------------------------
 -- created at: 2013-12-13
 
@@ -20,6 +21,17 @@ local Gesture = class('Gesture')
 -- CONSTANTS
 -- ======
 
+Gesture.static.State = {
+  "Possible" = "possible",
+  "Recongnized" = "recongnized",
+  "Failed" = "failed",
+  "Cancelled" = "cancelled",
+  -- state for continuous gestures
+  "Began" = "began",
+  "Changed" = "changed",
+  "Ended" = "ended",
+}
+
 -- ======
 -- VARIABLES
 -- ======
@@ -33,6 +45,11 @@ local Gesture = class('Gesture')
 -- ------
 
 function Gesture:initialize(opt)
+  system.activate("multitouch") -- ensure multitouch be actived
+
+  self.state = nil
+  -- control path of actions in gesture recognition
+
 end
 
 -- ------
